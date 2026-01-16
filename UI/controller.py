2963ef20141c7cbe848a0068d20e82f677fbc153
s316684 <s316684@studenti.polito.it>
2026-01-16 11:28:48 +0100

    commit di prova

--- UI/controller.py
+++ UI/controller.py
@@ -21,6 +21,7 @@ class Controller:
 
     def handle_crea_grafo(self, e):
         """ Handler per gestire creazione del grafo """
+        # prova
         # TODO
 
     def handle_best_prodotti(self, e):
